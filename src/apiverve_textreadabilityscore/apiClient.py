bceaa2c0485e044a4e6dd322d12743040256aa6b
APIVerve Team <hello@apiverve.com>
2025-03-28 17:45:57 -0500

    Bug Fixes

--- src/apiverve_textreadabilityscore/apiClient.py
+++ src/apiverve_textreadabilityscore/apiClient.py
@@ -10,10 +10,10 @@ class ReadabilityscoreAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/readabilityscore'
+        self.base_url = 'https://api.apiverve.com/v1/readabilityscore'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
